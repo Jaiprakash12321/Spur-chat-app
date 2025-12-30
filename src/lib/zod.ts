930c2a14b35de6dec00602615918ffa7b4ddbedd
Virajb19 <virajb853@gmail.com>
2025-12-31 05:07:12 +0530

    Increased username length limit

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const SignUpSchema = z.object({
-    username: z.string().min(3, {message: 'username must be atleast 3 letters long'}).max(10, {message: 'username cannot be more than 10 letters'}).trim(),
+    username: z.string().min(3, {message: 'username must be atleast 3 letters long'}).max(25, {message: 'username cannot be more than 10 letters'}).trim(),
     email: z.string().email({message: 'Please enter a valid email'}).trim(),
     password: z.string().min(8, {message: 'Password must be atleast 8 letters long'}).max(15)
               .regex(/^(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/, {message: 'Password must contain atleast one special char and one number'}).trim()
